afc05b0ea1edc73e1bf211ede3fc745c6fa19a53
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-05-04 11:25:04 +0100

    using links exercise

--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -3,10 +3,10 @@ require "rack/test"
 require_relative "../../app"
 
 def reset_albums_table
-    seed_sql = File.read('spec/seeds/albums_seeds.sql')
-    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
-    connection.exec(seed_sql)
-  end
+  seed_sql = File.read("spec/seeds/albums_seeds.sql")
+  connection = PG.connect({ host: "127.0.0.1", dbname: "music_library_test" })
+  connection.exec(seed_sql)
+end
 
 describe Application do
   # This is so we can use rack-test helper methods.
@@ -16,8 +16,7 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
-  
-  before(:each) do 
+  before(:each) do
     reset_albums_table
   end
 
@@ -28,8 +27,8 @@ describe Application do
 
       expect(response.status).to eq(200)
       expect(response.body).to eq("")
-      response = get('/albums')
-      expect(response.body).to include ('Voyage')
+      response = get("/albums")
+      expect(response.body).to include ("Voyage")
     end
   end
 
@@ -40,30 +39,40 @@ describe Application do
 
       expect(response.status).to eq(200)
       expect(response.body).to eq("")
-      response = get('/artists')
-      expect(response.body).to include ('Wild nothing')
+      response = get("/artists")
+      expect(response.body).to include ("Wild nothing")
     end
   end
 
-  context 'GET /albums/1' do 
-    it 'should return 200' do
-      response = get('/albums/1')
+  context "GET /albums/1" do
+    it "should return 200" do
+      response = get("/albums/1")
 
       expect(response.status).to eq 200
-      expect(response.body).to include('<h1>Doolittle</h1>')
-      expect(response.body).to include('Release year: 1989')
-      expect(response.body).to include('Artist: Pixies')
+      expect(response.body).to include("<h1>Doolittle</h1>")
+      expect(response.body).to include("Release year: 1989")
+      expect(response.body).to include("Artist: Pixies")
     end
   end
 
-  context 'GET /albums/2' do 
-    it 'should return 200' do
-      response = get('/albums/2')
+  context "GET /albums/2" do
+    it "should return 200" do
+      response = get("/albums/2")
 
       expect(response.status).to eq 200
-      expect(response.body).to include('<h1>Surfer Rosa</h1>')
-      expect(response.body).to include('Release year: 1988')
-      expect(response.body).to include('Artist: Pixies')
+      expect(response.body).to include("<h1>Surfer Rosa</h1>")
+      expect(response.body).to include("Release year: 1988")
+      expect(response.body).to include("Artist: Pixies")
+    end
+  end
+
+  context "GET /albums" do
+    it "returns list of albums" do
+      response = get("/albums")
+
+      expect(response.status).to eq 200
+      expect(response.body).to include('<a href="/albums/1">Doolittle</a>')
+      expect(response.body).to include('<a href="/albums/5">Bossanova</a>')
     end
   end
 end
